50eb19f14f254f23731a14ff4d9e89d774cbb2f1
Ben <bigriffin94@outlook.com>
2019-10-14 10:56:22 -0700

    middleware function added not working

--- index.js
+++ index.js
@@ -18,9 +18,10 @@ server.get('/', (req, res) => {
 
 
 server.post('/api/register', (req, res) => {
+
   let { username, password } = req.body;
 
-  const hash = bcrypt.hashSync(password)
+  const hash = bcrypt.hashSync(password, 8)
 
   Users.add({ username, password: hash })
     .then(saved => {
@@ -29,7 +30,8 @@ server.post('/api/register', (req, res) => {
     .catch(err => {
       res.status(500).json(err)
     })
-})
+});
+
 
 server.post('/api/login', (req, res) => {
   let { username, password } = req.body;
@@ -37,7 +39,7 @@ server.post('/api/login', (req, res) => {
   Users.findBy({ username })
     .first()
     .then(user => {
-      if (user) {
+      if (user && bcrypt.compareSync(password, user.password)) {
         res.status(200).json({ message: `Welcome ${user.username}!` });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
@@ -51,7 +53,7 @@ server.post('/api/login', (req, res) => {
 server.get('/api/users', (req, res) => {
   Users.find()
     .then(users => {
-      res.json(users);
+      res.status(200).json(users);
     })
     .catch(err => res.send(err));
 });
@@ -66,7 +68,15 @@ server.get('/hash', (req, res) => {
 
   // { hash: '970(&(:OHKJHIY*HJKH(*^)*&YLKJBLKJGHIUGH(*P' }
   res.send(`The hash for ${name} is ${hash}`)
-})
+});
+
+
+// implement the protected middleware that will check for username and password
+// in the headers and if valid provide access to the endpoint
+function protected(req, res, next) {
+
+}
+
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
